7fc1a9c2f42ab6837f24754ac03866860e24b658
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-09-13 18:00:25 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+import sequelize from './config/connection';
+// import sequelize connection (might be right)
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// sync sequelize models to the database, then turn on the server (Still need)
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
