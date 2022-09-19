632e09835c94adc4b2bc902693d24b3f5fe04785
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-09-18 21:34:30 -0600

    added more routes

--- api/category-routes.js
+++ api/category-routes.js
@@ -5,24 +5,69 @@ const { Category, Product } = require('../models');
 
 router.get('/', (req, res) => {
   // find all categories
+	Product.findAll({
+		include: [
+			Product, {
+				model: Category,
+				through: Category
+			}
+		]
+	})
   // be sure to include its associated Products
+	
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+	Product.findOne({
+		include: [
+			Category, {
+				model: Category,
+				through: Category
+			}
+		]
+	})
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+	Category.create(req.body)
+		.then((Category) => {
+		include: [
+			Category, {
+				model: Category,
+				through: Category
+			}
+		]
+	})
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+	console.log(req) 
+	 // update a category by its `id` value
+	Category.update(req.body, {
+		where: {
+			id: req.params.id,
+		},
+	})
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  console.log(req.params)
+	// delete a category by its `id` value
+	if(!req.params.id) res.status(400).json({ message: `cant not find product with ID ${req.params.id}`})
+	Product.delete({
+		where: {
+			id: req.params.id
+		},
+		include: [
+			Category, {
+				model: Category,
+				through: Category
+			}
+		]
+	})
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const Sequelize = require('sequelize');
+const { Category, Product, ProductTag } = require('./models');
 // import sequelize connection (might be right)
 
 const app = express();
@@ -10,6 +11,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+await Category.sync({ force: true });
+console.log("The table for the Category model was just (re)created!");
+
+await index.sync({ force: true });
+console.log("The index model was just (re)created!");
+
+await Product.sync({ force: true });
+console.log("The table for the Product model was just (re)created!");
+
+await ProductTag.sync({ force: true });
+console.log("The table for the ProductTag model was just (re)created!");
+
+await Tag.sync({ force: true });
+console.log("The table for the Tag model was just (re)created!");
 
 // sync sequelize models to the database, then turn on the server (Still need)
 app.listen(PORT, () => {
