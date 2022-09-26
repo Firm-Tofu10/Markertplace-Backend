da5a4fc5aee1b0ecf68cba6bf2e6c3568042631c
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-09-26 15:26:05 -0600

    Index.js associations
    Added to associations mostly copy past after the first one was done.

--- models/index.js
+++ models/index.js
@@ -4,18 +4,23 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-Category.hasMany(Product);
+Category.hasMany(Product, {
+	foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+	foreignKey: 'category_id',
+});
 // Categories have many Products
 Product.belongsToMany(Tag, { through: 
 	ProductTag
 })
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: 
-	ProductTag
-})
+Tag.belongsToMany(Product, { 
+	through: ProductTag,
+	foreignKey: 'tag_id'
+});
 module.exports = {
   Product,
   Category,
